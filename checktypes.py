a322a6824831578503f97a62dfe4927e82594135
maximegg <maximegg99@gmail.com>
2023-11-15 00:39:32 -0500

    fix

--- checktypes.py
+++ checktypes.py
@@ -2,17 +2,28 @@ import types
 
 class CheckTypesMeta(type):
 	@staticmethod
+	def check_arg_types(func, args, arg_types):
+		for arg_name, arg in zip(arg_types.keys(), args):
+			if not isinstance(arg, arg_types[arg_name]):
+				raise TypeError(f"L'argument {arg_name} n'est pas du type {arg_types[arg_name]}")
+
+	@staticmethod
+	def check_kwarg_types(kwargs, arg_types):
+		for key, value in kwargs.items():
+			if key in arg_types and not isinstance(value, arg_types[key]):
+				raise TypeError(f"L'argument {key} n'est pas du type {arg_types[key]}")
+
+	@staticmethod
 	def check_types_decorator(func):
 		def wrapper(self, *args, **kwargs):
 			arg_types = func.__annotations__
-			if not arg_types:
-				raise TypeError("La méthode n'est pas typée")
-			for arg_name, arg in zip(arg_types.keys(), args):
-				if not isinstance(arg, arg_types[arg_name]):
-					raise TypeError(f"L'argument {arg_name} n'est pas du type {arg_types[arg_name]}")
-			for key, value in kwargs.items():
-				if key in arg_types and not isinstance(value, arg_types[key]):
-					raise TypeError(f"L'argument {key} n'est pas du type {arg_types[key]}")
+			arg_names = list(func.__code__.co_varnames)
+			arg_names.remove("self")
+			for arg_name in arg_names:
+				if arg_name not in arg_types:
+					raise TypeError(f"L'argument {arg_name} n'est pas typé")
+			CheckTypesMeta.check_arg_types(func, args, arg_types)
+			CheckTypesMeta.check_kwarg_types(kwargs, arg_types)
 			return func(self, *args, **kwargs)
 		return wrapper
 
